609351d5859b0d643b9134d32cae972932ac7b34
likithshekar <likithshekar@users.noreply.github.com>
2020-02-09 19:35:17 +0530

    Commit

---
+++ input.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+//This is a simple single line comment
+
+int main(){
+    int a = 0;
+    if(a>=0){
+        /* This is 
+        a simple multiline comment
+        */
+        printf("If statement works");
+    }
+    for(int i=0;i<3;i++){
+        printf("%d. For Loop works\n",i);
+    }
+    while(a=0&&i=3){
+        printf("While statement works");
+    }
+}
